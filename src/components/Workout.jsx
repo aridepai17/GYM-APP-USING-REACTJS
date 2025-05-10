14bbc787ebb24614f1900a6daf02f333c6ccbeb8
aridepai17 <advaithdepai26@gmail.com>
2025-05-10 14:27:00 +0530

    ExerciseCard and Workout files

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
 
 export default function Workout() {
-  return (
-    <div>
-      Workout
-    </div>
-  )
+    const {workout} = props
+    return (
+        <SectionWrapper id={'workout'} header={'Welcome to'} title={['The', 'DANGER', 'zone']}>
+            <div className='flex flex-col gap-4'>
+                {workout.map((exercise, i) => {
+                    return (
+                        <ExerciseCard i={i} exercise={exercise} key={i} />
+                    )
+                })}
+            </div>
+        </SectionWrapper>
+    )
 }
